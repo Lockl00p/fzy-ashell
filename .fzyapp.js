e0e9db83cfb53d4db9b3c9ba224b2bc7a5e33086
Lockl00p <97256723+Lockl00p@users.noreply.github.com>
2024-06-08 09:41:48 -0700

    Fix temp file not removing sometimes and removing other times

--- .fzyapp.js
+++ .fzyapp.js
@@ -98,7 +98,7 @@ const ashell = {
 // We need the 'require' function:
 ashell.system('echo "$APPDIR" > ~/Documents/axpder')
 var requireCode = ashell.readFile(ashell.readFile("~/Documents/axpder") + "/require.js");
-ashell.deleteFile("~/Documents/axpder");
+ashell.system("rm ~/Documents/axpder");
 eval(requireCode);
 Tarp.require({expose: true}); 
 
