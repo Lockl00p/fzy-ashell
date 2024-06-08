eebfd4b2d77584a3b711bcc9e9a4fc77e57fb6b6
Lockl00p <97256723+Lockl00p@users.noreply.github.com>
2024-06-08 09:18:31 -0700

    Another attempt at the fix

--- .fzyapp.js
+++ .fzyapp.js
@@ -96,9 +96,9 @@ const jsc = {
 };
 
 // We need the 'require' function:
-jsc.system('echo "$APPDIR" > ~/tmp/.axpder')
-var requireCode = jsc.readFile(jsc.readFile("~/tmp/.axpder") + "/require.js");
-jsc.deleteFile("~/tmp/.axpder");
+jsc.system('echo "$APPDIR" > ~/Documents/.axpder')
+var requireCode = jsc.readFile(jsc.readFile("~/Documents/.axpder") + "/require.js");
+jsc.deleteFile("~/Documents/.axpder");
 eval(requireCode);
 Tarp.require({expose: true}); 
 
